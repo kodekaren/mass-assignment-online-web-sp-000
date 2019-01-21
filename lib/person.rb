72e01f887d388eb5cdd58f403f8053382fc52549
karen ercoli <karenercoli@gmail.com>
2019-01-21 18:41:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+    attr_accessor :name, :age, :location, :user_name
+   
+    def initialize(attributes)
+      @user_name = user_name
+      @name = name
+      @location = location
+      @age = age
+    end
+  end  #your code here
 end
